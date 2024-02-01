5cad612c3dc64bd427a445773587f124eed86097
Tomas Miranda <tomas.miranda@alu.ing.unlp.edu.ar>
2024-02-01 02:36:15 -0300

    minor login improvements. signup module implemented

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from output import *
 from utilities import path, startup, database_init
-from modules import login
+from modules import login,signup
 
 dir = path()
 settings = startup(dir)
@@ -17,14 +17,30 @@ while settings.outer_loop ==  True:
         if option == 0:
             ...
         elif option == 1:
-            option_title(option)
-            if login(dir,settings):
+            results = login(dir, settings, option)
+            if results == True:
                 success()
                 break
+            elif results == 'try_limit':
+                print(f"\t Ha superado el límite de intentos ({settings.tries}). Vuelva a intentarlo mas tarde")
+                div()
+                break
+            elif results == 'error_limit':
+                print(f"     Ha superado la cantidad de errores permitidos ({settings.errors}). Vuelva a intentarlo mas tarde")
+                div()
+                break
         elif option == 2:
-            ...
+            results = signup(dir, settings, option)
+            if results == True:
+                print(f"\t Ha superado el límite de intentos ({settings.tries}). Vuelva a intentarlo mas tarde")
+                div()
+                break
         elif option == 3:
             ...
         elif option == 4:
             option_title(option)
             settings.outer_loop=exit()
+        settings.inner_loop = False
+
+# cd C:\Users\RTX\Desktop\python\50215\entrega_1\
+# py main.py
--- modules.py
+++ modules.py
@@ -1,11 +1,35 @@
 import json
 from output import *
 
-def login(path, settings):
-    settings.inner_loop = True
+def credentials():
+    user=str(input("Ingrese nombre de usuario: "))
+    pw = str(input("Ingrese password: "))
+    return [user, pw]
+
+def go_back(settings):
+    choice = str(input("\t\t\tDesea volver al menu principal? (y/n): "))
+    print(choice)
+    if choice == 'y':
+        settings.errors = 0
+        settings.tries = 0
+        return True
+    else:
+        return False
+
+def reset_feature():
+    div()
+    print('\tPara habilitar los reinicios de constraseña, debe agregar un dato adicional.')
+    choice = str(input('\t\tEsta opción no es obligatoria. Desea activarla? (y/n): '))
+    if choice == 'y':
+        div()
+        return True
+
+def login(path, settings, option):
     with open(path + '/database.json') as database:
         db = json.load(database)
+        settings.inner_loop = True
     while settings.inner_loop == True:
+        option_title(option)
         user_data = credentials()
         find = user_data[0] in db['clientes']
         if find == True:
@@ -14,40 +38,86 @@ def login(path, settings):
                 return True
             else:
                 settings.tries+=1
-                if settings.tries < settings.try_limit:
+                if settings.tries < (settings.try_limit - 1):
                     div()
-                    print("\tHa ingresado una contrasenia incorrecta, por favor vuelva a intentarlo")
+                    print("\tHa ingresado una contraseña incorrecta, por favor vuelva a intentarlo")
                     div()
-                    goback = input("\t\t\tDesea volver al menu principal? (y/n): ")
-                    if goback == 'y':
+                    if go_back(settings):
                         settings.inner_loop = False
-                    
-                else:
-                    clear()
+                elif settings.tries == (settings.try_limit - 1):
                     div()
-                    print(f"Ha superado el límite de intentos. Vuelva a intentarlo mas tarde")
+                    print("\t\t\tEsta por alcanzar el limite de intentos.")
+                    div()
+                    if go_back(settings):
+                        settings.inner_loop = False
+                else:
+                    div()
                     settings.inner_loop = False
+                    return 'try_limit'
         else:
             settings.errors+=1
-            if settings.errors < settings.error_limit:
+            if settings.errors < (settings.error_limit - 1):
                 div()
                 print("\t\tNo existe el usuario ingresado. Por favor vuelva a intentarlo")
                 div()
-                goback=input("\t\t\tDesea volver al menu principal? (y/n): ")
-                if goback == 'y':
+                if go_back(settings):
                     settings.inner_loop = False
+            elif settings.errors == (settings.error_limit - 1):
                 div()
+                print("\t\t\tEsta por alcanzar el limite de intentos.")
+                div()
+                if go_back(settings):
+                    settings.inner_loop = False
             else:
-                print(f"Ha superado el límite de intentos. Vuelva a intentarlo mas tarde")
+                div()
                 settings.inner_loop = False
+                return 'error_limit'
 
-def signup():
-    ...
+def signup(path, settings, option):
+    with open(path + '/database.json') as database:
+        db = json.load(database)
+        settings.inner_loop = True
+    while settings.inner_loop == True:
+        option_title(option)
+        user_data = credentials()
+        user_check = user_data[0] in db['clientes']
+        if user_check == False:
+            if reset_feature():
+                print('\t\t\tEl dato de recuperacion sera su nombre real.')
+                auth = str(input('\t\t\tIngrese su dato de recuperacion: '))
+                div()
+                user_data.append(auth)
+                with open(path + '/database.json', 'w') as database:
+                    db['clientes'].update({user_data[0]:{'pw':user_data[1], 'name':user_data[2]}})
+                    json.dump(db, database)
+                    settings.inner_loop = False 
+            else:
+                div()
+                with open(path + '/database.json', 'w') as database:
+                    db['clientes'].update({user_data[0]:{'pw':user_data[1]}})
+                    json.dump(db, database)
+                    settings.inner_loop = False 
+        else:
+            settings.tries+=1
+            if settings.tries < (settings.try_limit - 1):
+                div()
+                print('\t\tEl usuario ingresado ya existe. Elija otro nombre de usuario.\n')
+                print('\t\t\tSi olvido su contraseña, puede restablecerla.\n')
+                print('\t\tEn caso contrario, pongase en contacto con el administrador.')
+                div()
+                if go_back(settings):
+                    settings.inner_loop = False                
+            elif settings.tries == (settings.try_limit - 1):
+                div()
+                print('\t\tEl usuario ingresado ya existe. Elija otro nombre de usuario.\n')
+                print("\t\t\t  Esta por alcanzar el limite de intentos.")
+                div()
+                if go_back(settings):
+                    settings.inner_loop = False
+            else:
+                div()
+                settings.inner_loop = False
+                return True
 
 def reset_password():
     ...
-
-def credentials():
-    user=str(input("Ingrese nombre de usuario: "))
-    pw = str(input("Ingrese password: "))
-    return [user, pw]
